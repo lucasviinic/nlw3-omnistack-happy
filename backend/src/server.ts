2288668bd1534f77529a6997933a4bc29fe147e8
Lucas Vinícius <lucas.vmsilva@ufrpe.br>
2020-10-17 16:25:15 -0300

    Inserção das views

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -1,10 +1,12 @@
-import express from 'express'
-import './database/connection'
-import routes from './routes'
+import express from 'express';
+import path from 'path';
+import './database/connection';
+import routes from './routes';
 
-const app = express()
+const app = express();
 
-app.use(express.json())
-app.use(routes)
+app.use(express.json());
+app.use(routes);
+app.use('/uploads', express.static(path.join(__dirname, '..', 'uploads')))
 
 app.listen(3333)
